52d342cf853ac49b244084c0794b6d86cf0286d1
Theis Ferré Hjortkjær <theisferre@Theiss-MacBook-Pro.local>
2021-08-04 12:54:25 +0200

    made very basic example of make_dataset

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -12,9 +12,18 @@ def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
+    print(input_filepath)
+    print(output_filepath)
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
-
+    outfile = open(output_filepath + "/output.txt", "w")
+    count = 1
+    with open(input_filepath + "/test.txt") as infile:
+        for line in infile:
+            outfile.write(line + str(count))
+            count += 1
+    
+    outfile.close()
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
