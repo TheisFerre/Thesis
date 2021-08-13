e5ebf7fad78f34b33bbe38ed9fa2c04ad40be3ee
Theis Ferré Hjortkjær <theisferre@Theiss-MacBook-Pro.local>
2021-08-13 08:59:13 +0200

    add function for computing distance adj matrix

--- src/data/process_dataset.py
+++ src/data/process_dataset.py
@@ -163,6 +163,42 @@ def features_and_targets(df: pd.DataFrame, region_ordering: List[str], id_col: s
     return X, targets
 
 
+def get_adjacency_matrix(distance_df, sensor_ids, normalized_k=0.1):
+    """
+    Creates distance based adjacency matrix.
+
+    TAKEN FROM https://github.com/sshleifer/Graph-WaveNet/blob/master/gen_adj_mx.py
+    :param distance_df: data frame with three columns: [from, to, distance].
+    :param sensor_ids: list of sensor ids.
+    :param normalized_k: entries that become lower than normalized_k after normalization are set to zero for sparsity.
+    :return:
+    """
+    num_sensors = len(sensor_ids)
+    dist_mx = np.zeros((num_sensors, num_sensors), dtype=np.float32)
+    dist_mx[:] = np.inf
+    # Builds sensor id to index map.
+    sensor_id_to_ind = {}
+    for i, sensor_id in enumerate(sensor_ids):
+        sensor_id_to_ind[sensor_id] = i
+
+    # Fills cells in the matrix with distances.
+    for row in distance_df.values:
+        if row[0] not in sensor_id_to_ind or row[1] not in sensor_id_to_ind:
+            continue
+        dist_mx[sensor_id_to_ind[row[0]], sensor_id_to_ind[row[1]]] = row[2]
+
+    # Calculates the standard deviation as theta.
+    distances = dist_mx[~np.isinf(dist_mx)].flatten()
+    std = distances.std()
+    adj_mx = np.exp(-np.square(dist_mx / std))
+    # Make the adjacent matrix symmetric by taking the max.
+    # adj_mx = np.maximum.reduce([adj_mx, adj_mx.T])
+
+    # Sets entries that lower than a threshold, i.e., k, to zero for sparsity.
+    adj_mx[adj_mx < normalized_k] = 0
+    return sensor_ids, sensor_id_to_ind, adj_mx
+
+
 class Dataset:
     def __init__(self, adjacency_matrix: np.array, targets: np.array, X: np.array):
         self.adjacency_matrix = adjacency_matrix
